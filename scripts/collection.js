605bb7d48c01766e70d2c8b6801c79c6bc1486ef
falk <falk.schwiefert@gmail.com>
2017-02-13 23:09:24 -0800

    started with intro and buy screen and buy functionality, incomlete and loads of code duplication, needs refactor once complete

---
+++ scripts/collection.js
@@ -0,0 +1,82 @@
+const playerOne = {
+    moneyLeft: 5000,
+    units: [],
+    buyActive: true,
+
+}
+
+const playerTwo = {
+    moneyLeft: 5000,
+    units: [],
+    buyActive: true,
+}
+
+const soldierUnit = {
+    id: null,
+    type: 'soldier',
+    movement: 5,
+    radius: 1,
+    armor: 1,
+    dmg: 4,
+    hp: 5,
+    movementAvailable: null,
+    attackAvailable: null,
+    alive: true,
+    price: 300,
+}
+
+const cavalryUnit = {
+    id: null,
+    type: 'cavalry',
+    movement: 7,
+    radius: 1,
+    armor: 0,
+    dmg: 4,
+    hp: 4,
+    movementAvailable: null,
+    attackAvailable: null,
+    alive: true,
+    price: 500,
+}
+
+const archerUnit = {
+    id: null,
+    type: 'archer',
+    movement: 4,
+    radius: 3,
+    armor: 1,
+    dmg: 3,
+    hp: 3,
+    movementAvailable: null,
+    attackAvailable: null,
+    alive: true,
+    price: 800,
+}
+
+const cannonUnit = {
+    id: null,
+    type: 'cannon',
+    movement: 2,
+    radius: 4,
+    armor: 3,
+    dmg: 6,
+    hp: 5,
+    movementAvailable: null,
+    attackAvailable: null,
+    alive: true,
+    price: 2000,
+}
+
+const knightUnit = {
+    id: null,
+    type: 'knight',
+    movement: 3,
+    radius: 1,
+    armor: 3,
+    dmg: 5,
+    hp: 5,
+    movementAvailable: null,
+    attackAvailable: null,
+    alive: true,
+    price: 1200,
+}
---
+++ scripts/main.js
@@ -0,0 +1,163 @@
+const gameState = {
+    startScreen: true,
+    shopScreen: false,
+    gameScreen: false,
+    winScreen: false,
+    loseScreen: false
+}
+
+function getGameState() {
+    const stateArr = Object.getOwnPropertyNames(gameState);
+    const activeState = stateArr.find(function(key) {
+        if (gameState[key]) {
+            return key
+        }
+    })
+    return activeState
+}
+
+function changeGameState(input) {
+    for (key in gameState) {
+        gameState[key] = false;
+        gameState[input] = true;
+    }
+    console.log(gameState)
+}
+
+function showScreen() {
+    $('.screen').hide()
+    console.log(getGameState())
+    switch(getGameState()) {
+        case 'shopScreen':
+            populatePrices()
+            $('#shopScreen').show()
+            break;
+        case 'gameScreen': $('#gameScreen').show()
+            break;
+        case 'winScreen': $('#winScreen').show()
+            break;
+        case 'loseScreen': $('#loseScreen').show()
+            break;
+        default: $('#startScreen').show()
+    }
+}
+
+function nextScreen() {
+    changeGameState('shopScreen')
+    showScreen()
+}
+
+//SHOP FUNCTIONS
+
+function populatePrices() {
+    $('#soldierPrice').html(soldierUnit.price)
+    $('#cavalryPrice').html(cavalryUnit.price)
+    $('#archerPrice').html(archerUnit.price)
+    $('#cannonPrice').html(cannonUnit.price)
+    $('#knightPrice').html(knightUnit.price)
+    if(playerOne.buyActive) {
+        $('#moneyLeftAmt').html(playerOne.moneyLeft)
+    } else if (playerTwo.buyActive) {
+        $('#moneyLeftAmt').html(playerTwo.moneyLeft)
+    }
+
+}
+
+function soldierBuy() {
+    if (playerOne.buyActive && playerOne.moneyLeft >= soldierUnit.price) {
+        playerOne.units.push(Object.assign(soldierUnit))
+        playerOne.units[playerOne.units.length-1].id = 'p1-' + playerOne.units.length
+        playerOne.moneyLeft -= soldierUnit.price
+    } else if (playerTwo.buyActive && playerTwo.moneyLeft >= soldierUnit.price) {
+        playerTwo.units.push(Object.assign(soldierUnit))
+        playerTwo.units[playerTwo.units.length-1].id = 'p1-' + playerTwo.units.length
+        playerTwo.moneyLeft -= soldierUnit.price
+    }
+    updateAmountsAndMoney()
+}
+
+function cavalryBuy() {
+    if (playerOne.buyActive && playerOne.moneyLeft >= cavalryUnit.price) {
+        playerOne.units.push(Object.assign(cavalryUnit))
+        playerOne.units[playerOne.units.length-1].id = 'p1-' + playerOne.units.length
+        playerOne.moneyLeft -= cavalryUnit.price
+    } else if (playerTwo.buyActive && playerTwo.moneyLeft >= cavalryUnit.price) {
+        playerTwo.units.push(Object.assign(cavalryUnit))
+        playerTwo.units[playerTwo.units.length-1].id = 'p1-' + playerTwo.units.length
+        playerTwo.moneyLeft -= cavalryUnit.price
+    }
+    updateAmountsAndMoney()
+}
+
+function archerBuy() {
+    if (playerOne.buyActive && playerOne.moneyLeft >= archerUnit.price) {
+        playerOne.units.push(Object.assign(archerUnit))
+        playerOne.units[playerOne.units.length-1].id = 'p1-' + playerOne.units.length
+        playerOne.moneyLeft -= archerUnit.price
+    } else if (playerTwo.buyActive && playerTwo.moneyLeft >= archerUnit.price) {
+        playerTwo.units.push(Object.assign(archerUnit))
+        playerTwo.units[playerTwo.units.length-1].id = 'p1-' + playerTwo.units.length
+        playerTwo.moneyLeft -= archerUnit.price
+    }
+    updateAmountsAndMoney()
+}
+
+function cannonBuy() {
+    if (playerOne.buyActive && playerOne.moneyLeft >= cannonUnit.price) {
+        playerOne.units.push(Object.assign(cannonUnit))
+        playerOne.units[playerOne.units.length-1].id = 'p1-' + playerOne.units.length
+        playerOne.moneyLeft -= cannonUnit.price
+    } else if (playerTwo.buyActive && playerTwo.moneyLeft >= cannonUnit.price) {
+        playerTwo.units.push(Object.assign(cannonUnit))
+        playerTwo.units[playerTwo.units.length-1].id = 'p1-' + playerTwo.units.length
+        playerTwo.moneyLeft -= cannonUnit.price
+    }
+    updateAmountsAndMoney()
+}
+
+function knightBuy() {
+    if (playerOne.buyActive && playerOne.moneyLeft >= knightUnit.price) {
+        playerOne.units.push(Object.assign(knightUnit))
+        playerOne.units[playerOne.units.length-1].id = 'p1-' + playerOne.units.length
+        playerOne.moneyLeft -= knightUnit.price
+    } else if (playerTwo.buyActive && playerTwo.moneyLeft >= knightUnit.price) {
+        playerTwo.units.push(Object.assign(knightUnit))
+        playerTwo.units[playerTwo.units.length-1].id = 'p1-' + playerTwo.units.length
+        playerTwo.moneyLeft -= knightUnit.price
+    }
+    updateAmountsAndMoney()
+}
+
+function updateAmountsAndMoney() {
+    const selected = {
+        soldier: 0,
+        cavalry: 0,
+        archer: 0,
+        cannon: 0,
+        knight: 0,
+        moneyLeft: 0
+    }
+    if (playerOne.buyActive) {
+        playerOne.units.forEach(function(unit) {
+            selected[unit.type]++
+        })
+        selected.moneyLeft = playerOne.moneyLeft
+    } else if (playerTwo.buyActive) {
+        playerTwo.units.forEach(function (unit) {
+            selected[unit.type]++
+        })
+        selected.moneyLeft = playerOne.moneyLeft
+    }
+    for (key in selected) {
+        $('#'+key+'Amt').html(selected[key])
+    }
+}
+
+
+
+
+
+
+
+
+showScreen();
